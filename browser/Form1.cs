ed49f2f97d1abe57f132833ed3627e435de5da01
Alexmelk <alexmelk@yandex.ru>
2019-09-09 21:20:46 +0300

    Подключил библиотеки, вывел первый сайт.

--- browser/Form1.cs
+++ browser/Form1.cs
@@ -7,14 +7,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using CefSharp;
+using CefSharp.WinForms;
 namespace browser
 {
     public partial class Form1 : Form
     {
+        public ChromiumWebBrowser chromeBrowser;
+
+        public void InitializeChromium()
+        {
+            CefSettings settings = new CefSettings();
+            // Initialize cef with the provided settings
+            Cef.Initialize(settings);
+            // Create a browser component
+            chromeBrowser = new ChromiumWebBrowser("http://ourcodeworld.com");
+            // Add it to the form and fill it to the form window.
+            this.Controls.Add(chromeBrowser);
+            chromeBrowser.Dock = DockStyle.Fill;
+        }
         public Form1()
         {
             InitializeComponent();
+            InitializeChromium();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+ 
+        }
+
+        private void ChromiumWebBrowser1_LoadingStateChanged(object sender, LoadingStateChangedEventArgs e)
+        {
+
         }
     }
 }
