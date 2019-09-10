5ab874257c208a643ac3663b7572f3c9de0d938c
Alexmelk <alexmelk@yandex.ru>
2019-09-10 21:49:04 +0300

    + удаление вкладок + картинки для назад и обновить + можно добавлять вкладки

--- browser/Form1.Designer.cs
+++ browser/Form1.Designer.cs
@@ -28,21 +28,122 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.metroTabControl1 = new MetroFramework.Controls.MetroTabControl();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // metroTabControl1
+            // 
+            this.metroTabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.metroTabControl1.Location = new System.Drawing.Point(21, 60);
+            this.metroTabControl1.Name = "metroTabControl1";
+            this.metroTabControl1.Size = new System.Drawing.Size(867, 489);
+            this.metroTabControl1.TabIndex = 0;
+            this.metroTabControl1.Selected += new System.Windows.Forms.TabControlEventHandler(this.MetroTabControl1_Selected);
+            this.metroTabControl1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.MetroTabControl1_MouseClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(95, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(247, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Enter += new System.EventHandler(this.TextBox1_Enter);
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(348, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(36, 31);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "GO";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.BackgroundImage = global::browser.Properties.Resources.refresh;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.Color.Transparent;
+            this.button3.Location = new System.Drawing.Point(55, 31);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(23, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.BackgroundImage = global::browser.Properties.Resources.f0da727195d739747a412a4153447b6f;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(26, 31);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(23, 23);
+            this.button2.TabIndex = 4;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // button4
+            // 
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button4.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(390, 25);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(73, 31);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Add Tab";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(909, 570);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.metroTabControl1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(21, 60, 21, 21);
+            this.Style = MetroFramework.MetroColorStyle.White;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private MetroFramework.Controls.MetroTabControl metroTabControl1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- browser/Form1.cs
+++ browser/Form1.cs
@@ -11,35 +11,117 @@ using CefSharp;
 using CefSharp.WinForms;
 namespace browser
 {
-    public partial class Form1 : Form
+    public partial class Form1 : MetroFramework.Forms.MetroForm
     {
-        public ChromiumWebBrowser chromeBrowser;
-
+        static volatile List<ChromiumWebBrowser> chromeTab = new List<ChromiumWebBrowser>();
+        static volatile List<string> Title = new List<string>();
+        static volatile public int selectedIndexTab;
         public void InitializeChromium()
         {
             CefSettings settings = new CefSettings();
-            // Initialize cef with the provided settings
             Cef.Initialize(settings);
             // Create a browser component
-            chromeBrowser = new ChromiumWebBrowser("http://ourcodeworld.com");
-            // Add it to the form and fill it to the form window.
-            this.Controls.Add(chromeBrowser);
-            chromeBrowser.Dock = DockStyle.Fill;
         }
         public Form1()
         {
             InitializeComponent();
             InitializeChromium();
-        }
 
+            timer1.Start();
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
- 
+
         }
 
-        private void ChromiumWebBrowser1_LoadingStateChanged(object sender, LoadingStateChangedEventArgs e)
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
+             Cef.Shutdown();
+        }
 
+        private void TextBox1_Enter(object sender, EventArgs e)
+        {
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            chromeTab[selectedIndexTab].Load(textBox1.Text);
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            chromeTab[selectedIndexTab].Back();
+        }
+
+        private async void Button3_Click(object sender, EventArgs e)
+        {
+            chromeTab[selectedIndexTab].Reload();
+            while (chromeTab[selectedIndexTab].IsLoading) await Task.Delay(3);
+        }
+        private void TitleChanged(object sender, TitleChangedEventArgs e)
+        {
+            if (chromeTab.Count > Title.Count){ Title.Add(e.Title); }
+            else { Title[selectedIndexTab] = e.Title;}
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            //metroTabPage1.Text = Title;
+            for(int i = 0; i < Title.Count; i++)
+            {
+                metroTabControl1.TabPages[i].Text = Title[i];
+            }   
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            metroTabControl1.TabPages.Add(new TabPage("tab "+metroTabControl1.TabPages.Count));
+
+            chromeTab.Add(new ChromiumWebBrowser("http://yandex.ru"));
+
+            metroTabControl1.TabPages[metroTabControl1.TabPages.Count-1].Controls.Add(chromeTab[chromeTab.Count-1]);
+            chromeTab[chromeTab.Count-1].Dock = DockStyle.Fill;
+
+            chromeTab[chromeTab.Count - 1].TitleChanged += new EventHandler<TitleChangedEventArgs>(TitleChanged);
+
+            textBox1.Text = chromeTab[chromeTab.Count - 1].Address;
+
+            metroTabControl1.SelectTab(chromeTab.Count - 1);
+
+            metroTabControl1.Update();
+        }
+
+        private void MetroTabControl1_Selected(object sender, TabControlEventArgs e)
+        {
+            try
+            {
+                textBox1.Text = chromeTab[e.TabPageIndex].Address;
+
+                selectedIndexTab = e.TabPageIndex;
+            }
+            catch { }
+        }
+
+        private void MetroTabControl1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if(e.Button==MouseButtons.Right) {
+                List<string> Titlebuf = new List<string>();
+                for (int i = 0; i < Title.Count; i++)
+                {
+                    if (i != selectedIndexTab) { Titlebuf.Add(Title[i]); }
+                }
+                Title.Clear(); Title.AddRange(Titlebuf); Titlebuf.Clear();
+
+                List<ChromiumWebBrowser> chromeTabBuf = new List<ChromiumWebBrowser>();
+                for (int i = 0; i < chromeTab.Count; i++)
+                {
+                    if (i != selectedIndexTab) { chromeTabBuf.Add(chromeTab[i]); }
+                }
+                chromeTab.Clear(); chromeTab.AddRange(chromeTabBuf); chromeTabBuf.Clear();
+
+                metroTabControl1.TabPages[selectedIndexTab].Dispose();
+
+            }
         }
     }
 }
